247d988dee4e1014204c3100bf8b0025bb9f9c4c
Daniel Berthran <berthlearn@gmail.com>
2024-11-01 13:13:54 +0100

    create 0-basic_cache for basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+A basic caching system
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+    A basic caching system
+    '''
+    def __init__(self):
+        '''
+        Construtor method
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        Assigns the item value to the key in the cache storage
+        '''
+        if not key and not item:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        Returns the value of the key
+        '''
+        value = self.cache_data.get(key)
+        return value
