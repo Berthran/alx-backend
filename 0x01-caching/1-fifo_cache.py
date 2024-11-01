2e356797168e3b9ca4e7827e49db14a160f73b3c
Daniel Berthran <berthlearn@gmail.com>
2024-11-01 13:48:24 +0100

    create FIFO algorithm for caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''
+A basic implementation of a FIFO caching system
+'''
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    A FIFO caching algorithm
+    '''
+
+    def __init__(self):
+        '''
+        Constructor method
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        Update or add items to cache_data
+        '''
+        if key is None or item is None:
+            pass
+        else:
+            if self.get(key) is None:
+                if len(self.cache_data) == self.MAX_ITEMS:
+                    keysReversed = sorted(self.cache_data.keys(), reverse=True)
+                    keyToDiscard = keysReversed[-1]
+                    print("DISCARD: ", keyToDiscard)
+                    self.cache_data.pop(keyToDiscard)
+                    self.cache_data.update({key: item})
+                else:
+                    self.cache_data.update({key: item})
+            else:
+                self.cache_data.update({key: item})
+
+    def get(self, key):
+        '''
+        Returns the Value of a Key in the cache_data
+        '''
+        return self.cache_data.get(key)
